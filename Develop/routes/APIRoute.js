ed0acf07c1ba49dbe1d40934cae627fe08f18e33
Kyler Gibbs <kylergibbs@hotmail.com>
2024-06-17 23:59:13 -0700

    Delete button functional

--- Develop/routes/APIRoute.js
+++ Develop/routes/APIRoute.js
@@ -7,7 +7,7 @@ router.get('/notes', async (req, res) => {
     const newData = JSON.parse(data);
     return res.json(newData);
        
-})
+});
 
 router.post('/notes', (req, res) => {
     const arr = fs.readFileSync('./db/db.json');
@@ -18,6 +18,17 @@ router.post('/notes', (req, res) => {
     const newData = JSON.parse(data);
     return res.json(newData);
 });
+
+router.delete('/notes/:id', async (req, res) => {
+    const arr = await fs.readFileSync('./db/db.json');
+    const data = JSON.parse(arr)
+    const remove = req.params.id
+    const db = data.filter(id => id.id !== remove)
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    const datas = fs.readFileSync('./db/db.json');
+    const newData = JSON.parse(datas);
+    return res.json(newData);
+})
 
 
 
