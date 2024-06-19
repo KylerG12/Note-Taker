9778392e43ac97a9ce6fd176c679d89bef1c95ee
Kyler Gibbs <kylergibbs@hotmail.com>
2024-06-18 21:50:09 -0700

    Deploy to Heroku.

--- Develop/routes/APIRoute.js
+++ Develop/routes/APIRoute.js
@@ -1,35 +1,32 @@
-const router = require('express').Router();
-const fs = require('fs');
-const uuid = require('uuid/v1');
+const router = require("express").Router();
+const fs = require("fs");
+const uuid = require("uuid/v1");
 
-router.get('/notes', async (req, res) => {
-    const data = await fs.readFileSync('./db/db.json');
-    const newData = JSON.parse(data);
-    return res.json(newData);
-       
+router.get("/notes", async (req, res) => {
+  const data = await fs.readFileSync("./db/db.json");
+  const newData = JSON.parse(data);
+  return res.json(newData);
 });
 
-router.post('/notes', (req, res) => {
-    const arr = fs.readFileSync('./db/db.json');
-    const par = JSON.parse(arr);
-    const add = [...par, {...req.body, id:uuid()}];
-    fs.writeFileSync('./db/db.json', JSON.stringify(add))
-    const data = fs.readFileSync('./db/db.json');
-    const newData = JSON.parse(data);
-    return res.json(newData);
+router.post("/notes", (req, res) => {
+  const arr = fs.readFileSync("./db/db.json");
+  const par = JSON.parse(arr);
+  const add = [...par, { ...req.body, id: uuid() }];
+  fs.writeFileSync("./db/db.json", JSON.stringify(add));
+  const data = fs.readFileSync("./db/db.json");
+  const newData = JSON.parse(data);
+  return res.json(newData);
 });
 
-router.delete('/notes/:id', async (req, res) => {
-    const arr = await fs.readFileSync('./db/db.json');
-    const data = JSON.parse(arr)
-    const remove = req.params.id
-    const db = data.filter(id => id.id !== remove)
-    fs.writeFileSync('./db/db.json', JSON.stringify(db))
-    const datas = fs.readFileSync('./db/db.json');
-    const newData = JSON.parse(datas);
-    return res.json(newData);
-})
-
-
+router.delete("/notes/:id", async (req, res) => {
+  const arr = await fs.readFileSync("./db/db.json");
+  const data = JSON.parse(arr);
+  const remove = req.params.id;
+  const db = data.filter((id) => id.id !== remove);
+  fs.writeFileSync("./db/db.json", JSON.stringify(db));
+  const datas = fs.readFileSync("./db/db.json");
+  const newData = JSON.parse(datas);
+  return res.json(newData);
+});
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const html = require('./routes/htmlRoute');
-const api = require('./routes/APIRoute');
+const html = require("./routes/htmlRoute");
+const api = require("./routes/APIRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use('/api', api);
-app.use('/', html);
+app.use(express.static("public"));
+app.use("/api", api);
+app.use("/", html);
 
-
-app.listen(PORT, () => console.log('Now listening'))
+app.listen(PORT, () => console.log("Now listening"));
